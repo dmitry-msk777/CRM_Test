38255f8aa52bda2bc678041ad541e1467798cdcf
dmitry-msk777 <51027665+dmitry-msk777@users.noreply.github.com>
2020-03-04 01:36:56 +0300

    1

--- src/main/CRM.go
+++ src/main/CRM.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"html/template"
 	"github.com/gorilla/mux"
 )
+
+type ViewData struct{
+    Title string
+    Message string
+}
 
 func productsHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -36,6 +41,16 @@ func postform(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Имя: %s Возраст: %s", name, age)
 }
 
+func templates(w http.ResponseWriter, r *http.Request) {
+ 
+	data := ViewData{
+		Title: "World Cup",
+		Message: "FIFA will never regret it",
+	}
+	tmpl, _ := template.ParseFiles("templates/templates.html")
+	tmpl.Execute(w, data)
+}
+
 func main() {
 
 	router := mux.NewRouter()
@@ -50,6 +65,9 @@ func main() {
 
 	router.HandleFunc("/users", users)
 	router.HandleFunc("/postform", postform)
+
+	router.HandleFunc("/templates", templates)
+
 
 	http.Handle("/", router)
 
