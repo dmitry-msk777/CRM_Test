5bdcd5b8e822eb7317b6ad52fbc6d1458aab5546
dmitry-msk777 <51027665+dmitry-msk777@users.noreply.github.com>
2020-03-09 04:13:59 +0300

    1

--- src/main/CRM.go
+++ src/main/CRM.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"encoding/gob"
+	"flag"
 	"fmt"
-	"net/http"
 	"html/template"
+	"net/http"
+
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
-type ViewData struct{
-    Title string
-    Message string
+const (
+	sesKeyLogin sesKey = iota
+)
+
+var cookieStore = sessions.NewCookieStore([]byte("secret"))
+
+const cookieName = "MyCookie"
+
+type sesKey int
+
+type ViewData struct {
+	Title   string
+	Message string
 }
 
 func productsHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,16 +56,61 @@ func postform(w http.ResponseWriter, r *http.Request) {
 }
 
 func templates(w http.ResponseWriter, r *http.Request) {
- 
+
 	data := ViewData{
-		Title: "World Cup",
+		Title:   "World Cup",
 		Message: "FIFA will never regret it",
 	}
 	tmpl, _ := template.ParseFiles("templates/templates.html")
 	tmpl.Execute(w, data)
 }
 
+func mainpage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Index Page")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	ses, err := cookieStore.Get(r, cookieName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	ses.Values[sesKeyLogin] = "user"
+	err = cookieStore.Save(r, w, ses)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func loginind(w http.ResponseWriter, r *http.Request) {
+	ses, err := cookieStore.Get(r, cookieName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	login, ok := ses.Values[sesKeyLogin].(string)
+	if !ok {
+		login = "anonymous"
+	}
+
+	w.Write([]byte("you are " + login))
+}
+
+func logintest(w http.ResponseWriter, r *http.Request) {
+	url := "login/login.html" //Страница входа
+	if len(r.Header["Cookie"]) != 0 && r.Header["Cookie"][0] == "auth=your_MD5_cookies" {
+		url = "login/index.html" //Страница после успешной авторизации
+	}
+	t, _ := template.ParseFiles(url)
+	t.Execute(w, "")
+}
+
 func main() {
+
+	gob.Register(sesKey(0))
 
 	router := mux.NewRouter()
 
@@ -68,6 +127,21 @@ func main() {
 
 	router.HandleFunc("/templates", templates)
 
+	router.HandleFunc("/mainpage", mainpage)
+
+	router.HandleFunc("/login", login)
+
+	router.HandleFunc("/loginind", loginind)
+
+	router.HandleFunc("/logintest", logintest)
+
+	var dir string
+	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+	flag.Parse()
+
+	//router.Handle("/js/", http.FileServer(http.Dir("./js/")))
+	//router.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("/login/js/"))))
+	router.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("./js/"))))
 
 	http.Handle("/", router)
 
